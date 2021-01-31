90f168d7f95940c5a3764a8217fbe43fc8ca4ad5
PratikFandade <pratik.fandade18@vit.edu>
2021-01-31 11:55:16 +0530

    New Home Page Update

--- js/main.js
+++ js/main.js
@@ -4,24 +4,32 @@ if (screen.width > parseInt("880px")) {
 
     document.getElementById("contactUsForm").style.width = "30%";
 
-    document.getElementById("contactus-iconId").style.marginRight = "30%";
-    document.getElementById("contactus-iconId").style.opacity = 0;
+    document.getElementById("socialMedia-icons").style.marginRight = "30%";
+    document.getElementById("socialMedia-icons").style.opacity = 0;
+    document.getElementById("marketing-line").style.marginRight = "30%";
+    document.getElementById("marketing-line").style.opacity = 0;
+    document.getElementById("contact-btn-wrap").style.marginRight = "30%";
+    document.getElementById("contact-btn-wrap").style.opacity = 0;
   }
 
   function closeForm() {
     document.getElementById("contactUsForm").style.width = "0";
     document.getElementById("contactUsForm").style.opacity = 0;
 
-    document.getElementById("contactus-iconId").style.marginRight = "0px";
-    document.getElementById("contactus-iconId").style.opacity = 1;
+    document.getElementById("socialMedia-icons").style.marginRight = "0px";
+    document.getElementById("socialMedia-icons").style.opacity = 1;
+    document.getElementById("marketing-line").style.marginRight = "0px";
+    document.getElementById("marketing-line").style.opacity = 1;
+    document.getElementById("contact-btn-wrap").style.marginRight = "0px";
+    document.getElementById("contact-btn-wrap").style.opacity = 1;
   }
 }
 if (screen.width <= parseInt("880px")) {
   function openForm() {
     document.getElementById("contactUsForm").style.opacity = 1;
     document.getElementById("contactUsForm").style.width = "100%";
-    document.getElementById("contactus-iconId").style.marginRight = "100%";
-    document.getElementById("contactus-iconId").style.opacity = 0;
+    document.getElementById("socialMedia-icons").style.marginRight = "100%";
+    document.getElementById("socialMedia-icons").style.opacity = 0;
 
     document.getElementById("mobile-nav").style.height = 0;
   }
@@ -30,8 +38,8 @@ if (screen.width <= parseInt("880px")) {
     document.getElementById("contactUsForm").style.width = "0";
     document.getElementById("contactUsForm").style.opacity = 0;
 
-    document.getElementById("contactus-iconId").style.marginRight = "0px";
-    document.getElementById("contactus-iconId").style.opacity = 1;
+    document.getElementById("socialMedia-icons").style.marginRight = "0px";
+    document.getElementById("socialMedia-icons").style.opacity = 1;
     document.getElementById("mobile-nav").style.height = "auto";
   }
 }
