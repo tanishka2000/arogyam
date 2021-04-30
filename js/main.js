7446028a1b33639da5dedaaef7d39bef48a43af6
Vishal Khandate <vishukblog000@gmail.com>
2021-04-30 22:34:24 +0530

    + opacity replaced by display

--- js/main.js
+++ js/main.js
@@ -3,12 +3,8 @@ function isMobile() {
 }
 
 function openForm() {
-  // closeFaq();
-  document.getElementById("contactUsForm").style.opacity = isMobile() ? 1 : 1;
+  document.getElementById("contactUsForm").style.opacity = 1;
 
-  // document.getElementById("contactUsForm").style.width = isMobile()
-  //   ? "100%"
-  //   : "30%";
   document.getElementById("contactUsForm").style.width = isMobile()
     ? "100%"
     : "455px";
@@ -16,20 +12,14 @@ function openForm() {
   document.getElementById("socialMedia-icons").style.marginRight = isMobile()
     ? "100%"
     : "30%";
-  document.getElementById("socialMedia-icons").style.opacity = isMobile()
-    ? 0
-    : 0;
+  document.getElementById("socialMedia-icons").style.opacity = 0;
+  document.getElementById("contact-btn-wrap").style.marginRight = "30%";
+  document.getElementById("contact-btn-wrap").style.display = "none";
   // for big screen
   if (!isMobile()) {
     document.getElementById("marketing-line").style.marginRight = "30%";
     document.getElementById("marketing-line").style.opacity = 0;
-    document.getElementById("contact-btn-wrap").style.marginRight = "30%";
-    document.getElementById("contact-btn-wrap").style.opacity = 0;
   }
-  // // for mobile screen
-  // if (isMobile()) {
-  //   document.getElementById("mobile-nav").style.height = 0;
-  // }
 }
 
 function closeForm() {
@@ -38,17 +28,14 @@ function closeForm() {
 
   document.getElementById("socialMedia-icons").style.marginRight = "0px";
   document.getElementById("socialMedia-icons").style.opacity = 1;
+
+  document.getElementById("contact-btn-wrap").style.marginRight = "0px";
+  document.getElementById("contact-btn-wrap").style.display = "block";
   // for big screen
   if (!isMobile()) {
     document.getElementById("marketing-line").style.marginRight = "0px";
     document.getElementById("marketing-line").style.opacity = 1;
-    document.getElementById("contact-btn-wrap").style.marginRight = "0px";
-    document.getElementById("contact-btn-wrap").style.opacity = 1;
   }
-  // // for mobile screen
-  // if (isMobile()) {
-  //   document.getElementById("mobile-nav").style.height = "auto";
-  // }
 }
 
 function closeErrorAlert() {
@@ -67,14 +54,14 @@ function closeSuccessAlert() {
 
 function openFaq() {
   closeForm();
-  document.getElementById("contact-btn-wrap").style.opacity = 0;
+  document.getElementById("contact-btn-wrap").style.display = "none";
   // document.getElementById("faqSection").style.opacity = 1;
   document.getElementById("faqSection").style.width = isMobile()
     ? "100%"
     : "30%";
 }
 function closeFaq() {
-  document.getElementById("contact-btn-wrap").style.opacity = 1;
+  document.getElementById("contact-btn-wrap").style.display = "block";
   // document.getElementById("faqSection").style.opacity = 0;
   document.getElementById("faqSection").style.width = "0%";
 }
